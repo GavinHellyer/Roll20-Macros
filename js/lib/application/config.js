d0f94f9122c8f8424bf3d80a57a9e0ff1be68e1a
Gavin Hellyer <galitayer@gmail.com>
2012-10-23 23:50:50 +0100

    lib
    adding the application config and general libs

---
+++ js/lib/application/config.js
@@ -0,0 +1,14 @@
+(function () {
+    define(function () {
+        var config = {
+            db: {
+                name: 'Roll20',
+                version: '',
+                description: 'Roll20 Macros Database',
+                size: 2 * 1024 * 1024
+            }
+        };
+        
+        return config;
+    });
+}());
