1aaccf6cbbaec04f9f43e10d0c69a4083928bc4e
Gavin Hellyer <galitayer@gmail.com>
2012-10-24 21:25:15 +0100

    Merged databaseMigrator into databaseSetup

--- js/Models/databaseSetup.js
+++ js/Models/databaseSetup.js
@@ -4,12 +4,11 @@
  * Setup the database
  * 
  */
-define(['model/databaseMigrator'], function(databaseMigrator) {
+define([], function() {
     var dbSetup = function() {
         /*
          * Default Class Variables
          */
-        this.databaseMigrator = new databaseMigrator();
         this._version = 1.1;
         this._schema = {
             1: {
@@ -20,7 +19,13 @@ define(['model/databaseMigrator'], function(databaseMigrator) {
                 ]
             }
         };
-            
+        
+        // Pending migrations to run
+        var migrations = [];
+        // Callbacks to run when migrations done
+        var done = [];
+        var state = 0;
+
         /*
          * Sync the schema
          * Loops through the schema changes and
@@ -29,6 +34,10 @@ define(['model/databaseMigrator'], function(databaseMigrator) {
          * @return status bool
          */
         this.sync = function() {
+            if (state > 0) {
+                throw "Migrator is only valid once -- create a new one if you want to do another migration.";
+            }
+            
             // Get current database version
             var currentVersion = parseFloat(app.dbAdapter.db.version);
             
@@ -51,15 +60,64 @@ define(['model/databaseMigrator'], function(databaseMigrator) {
                             }
                         };
                         
-                        this.databaseMigrator.migration(update);
+                        migrations.push(update);
                     }
                 }
             }
             
-            this.databaseMigrator.execute();
+            state = 1;
+            try {
+                doMigration();
+            }
+            catch(e) {
+                error(e);
+            }
             
             return;
         };
+        
+        // Called when the migration has completed.  If the migration has already completed,
+        // executes immediately.  Otherwise, waits.
+        this.done = function(func) {
+            if (typeof func !== "array") {
+                func = [func];
+            }
+            
+            for(var f in func) {
+                done.push(func[f]);
+            }
+            
+            if (state > 1) {
+                executeDoneCallbacks();
+            }
+        };
+
+        // Execute a migration
+        var doMigration = function() {
+            if (migrations[0]) {
+                app.dbAdapter.db.transaction(function(t) {
+                    var update = migrations[0];
+                    app.dbAdapter.db.changeVersion(update.version, update.nextVersion, function(t) {
+                        update.execute(t);
+                    }, function (err) {
+                        console.log(err.message);
+                    });
+                    delete(migrations[0]);
+                    doMigration();
+                }, function (err) {
+                    console.log(err.message);
+                });
+            } else {
+                state = 2;
+                executeDoneCallbacks();
+            }
+        };
+
+        var executeDoneCallbacks = function() {
+            for(var f in done) {
+                done[f]();
+            }
+        }
     };
     
     return dbSetup;
