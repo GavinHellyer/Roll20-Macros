d9f74ad19a6feb37fcd95ab066aa4114928a397c
Gavin Hellyer <galitayer@gmail.com>
2012-10-27 23:36:11 +0100

    Added ucfirst, lcfirst, ucwords

--- js/lib/application/functions.js
+++ js/lib/application/functions.js
@@ -1,3 +1,21 @@
 var notify = function(msg, type) {
     console.log(msg);
 };
+
+var ucfirst = function(str) {
+    str += '';
+    var f = str.charAt(0).toUpperCase();
+    return f + str.substr(1);
+};
+
+var lcfirst = function(str) {
+    str += '';
+    var f = str.charAt(0).toLowerCase();
+    return f + str.substr(1);
+};
+
+var ucwords = function(str) {
+    return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+        return $1.toUpperCase();
+    });
+};
